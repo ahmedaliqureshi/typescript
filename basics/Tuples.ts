bebb67b6a90ea78fcfd479296f9fee4af49252fc
Ahmed <ahmedaliqureshi59@gmail.com>
2026-09-12 19:12:21 +0000

    tuples

---
+++ basics/Tuples.ts
@@ -0,0 +1,14 @@
+//typles are the typed array and they let us define each element of the array, the order and the type 
+//of the defined type and the elements matter
+
+let tuple:[string,boolean,number];
+tuple=["abc",true,1]; // make sure it is in the order
+console.log(tuple);
+
+// TUPLES METHODS
+
+console.log(tuple.at(0));
+console.log(tuple.length); 
+
+
+
